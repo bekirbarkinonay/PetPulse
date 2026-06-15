f0b9de89c62e57ec92797b5a0f04a7cdd12ebb7b
bekirbarkinonay <onaybekirbarkin@gmail.com>
2026-06-14 22:20:45 -0400

    ok

--- Backend/PetPulse.API/PetPulse.API/Models/User.cs
+++ Backend/PetPulse.API/PetPulse.API/Models/User.cs
@@ -1,18 +1,19 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace PetPulse.API.Models
+public class User 
 {
-    public class User
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
-        public string FirstName { get; set; } = ""; // Yeni
-        public string LastName { get; set; } = "";  // Yeni
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string Role { get; set; } = "User";
-    }
+    [BsonElement("email")] // Veritabanında her zaman küçük harf 'email' olarak sakla
+    public string Email { get; set; }
+
+    [BsonElement("password")] // Veritabanında her zaman küçük harf 'password' olarak sakla
+    public string Password { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
 }
