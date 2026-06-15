7f9009b203f003bcbca797e8d0a860bf74619e82
bekirbarkinonay <onaybekirbarkin@gmail.com>
2026-06-15 10:39:30 -0400

    solved

--- Backend/PetPulse.API/PetPulse.API/Models/User.cs
+++ Backend/PetPulse.API/PetPulse.API/Models/User.cs
@@ -7,12 +7,11 @@ public class User
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("email")] // Veritabanında her zaman küçük harf 'email' olarak sakla
-    public string Email { get; set; }
+    [BsonElement("Email")]    // büyük E
+public string Email { get; set; }
 
-    [BsonElement("password")] // Veritabanında her zaman küçük harf 'password' olarak sakla
-    public string Password { get; set; }
-
+[BsonElement("Password")] // büyük P
+public string Password { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Role { get; set; }
